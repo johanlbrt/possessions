b411b7925273f8a6c8a7057698bf9ce2ce09a093
Johan Liebert <bggbss@rambler.ru>
2023-08-13 01:21:54 +0300

    Correctly copying a string to preserve the original string value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    // let s2 = s1; || bad because s1 will be not exist
+    // we need add s1.clone to save s1
+    let s2 = s1.clone();
+    println!("{s1}{s2}"); //hellohello
 }
