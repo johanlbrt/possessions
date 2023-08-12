e8e377bee4c8741c486bfe78d7b7d72be4b40465
Johan Liebert <bggbss@rambler.ru>
2023-08-13 01:34:31 +0300

    Use turp for clone string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let s1 = String::from("hello");
-    // let s2 = s1; || bad because s1 will be not exist
-    // we need add s1.clone to save s1
-    let s2 = s1.clone();
-    println!("{s1}{s2}"); //hellohello
+    let anything: (u8, &str) = (42, "Hello");
+    println!("{}", anything.1);
+    let some_text = anything.1;
+    println!("{}", some_text);
 }
